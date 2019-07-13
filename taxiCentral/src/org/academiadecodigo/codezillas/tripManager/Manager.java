fcef50611ae2ccb4b5b60aa294393de4ea1c6332
Horacio Correia <rmcorre@gmail.com>
2019-07-13 10:47:28 +0000

    Add changes from remote

--- taxiCentral/src/org/academiadecodigo/codezillas/tripManager/Manager.java
+++ taxiCentral/src/org/academiadecodigo/codezillas/tripManager/Manager.java
@@ -1,9 +1,35 @@
 package org.academiadecodigo.codezillas.tripManager;
 
     //TODO: Implement trip (a.k.a. "Corrida") management logic
-    //Could contain a client list as well as a taxi list if there is no need to create separate ClientList class
+    //Could contain a client list as well as a taxi list if there
+    // is no need to create separate ClientList class
+
+import org.academiadecodigo.codezillas.user.Client;
+import org.academiadecodigo.codezillas.user.Driver;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 
 public class Manager {
+    private Client client;
+    private Driver[] driver;
+    private double cost = 8;
+    private PrintWriter out;
+    private BufferedReader in;
 
-    private double cost;
+    public void Manager(Client client, Driver[] driver) {
+        this.client = client;
+        this.driver = driver;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+//
+
 }
