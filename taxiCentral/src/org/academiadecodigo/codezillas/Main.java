09d48884793da6d717f81cf4d791347c96b7ecec
Miguel Machado <positifproject@gmail.com>
2019-07-13 00:06:16 +0000

    starting menus

--- taxiCentral/src/org/academiadecodigo/codezillas/Main.java
+++ taxiCentral/src/org/academiadecodigo/codezillas/Main.java
@@ -1,12 +1,14 @@
 package org.academiadecodigo.codezillas;
 
-    //TODO: Main class should only instantiate and execute the class that handles all program logic
-    //TODO: Nested psvm can be used elsewhere to execute the whole program and this one can be used for testing and deleted later
+//TODO: Main class should only instantiate and execute the class that handles all program logic
+//TODO: Nested psvm can be used elsewhere to execute the whole program and this one can be used for testing and deleted later
 
-    //TODO: IMPORTANT: Try to implement Design Patterns and interface-based programming and code conventions
+//TODO: IMPORTANT: Try to implement Design Patterns and interface-based programming and code conventions
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Boas");
     }
+
 }
--- taxiCentral/src/org/academiadecodigo/codezillas/menu/MenuPrompts.java
+++ taxiCentral/src/org/academiadecodigo/codezillas/menu/MenuPrompts.java
@@ -1,11 +1,46 @@
 package org.academiadecodigo.codezillas.menu;
 
-    //TODO: Implement Prompt-view menus and inputs here
-    //TODO (associated logic can be put in adjacent class MenuLogic if the methods become too large)
-    //TODO This class will need to "know" the main logic handler class in order to interact
+//TODO: Implement Prompt-view menus and inputs here
+//TODO (associated logic can be put in adjacent class MenuLogic if the methods become too large)
+//TODO This class will need to "know" the main logic handler class in order to interact
 
-    //Symbolic "login" menu and "Request taxi" menus
+//Symbolic "login" menu and "Request taxi" menus
+
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+
 
 public class MenuPrompts {
+
+    Prompt prompt = new Prompt(System.in, System.out);
+
+
+    public int login() {
+
+        System.out.println("WELCOME TO CENTRAL CRAZY TAXI");
+        String[] options = {"Login", "Exit"};
+        MenuInputScanner scanner = new MenuInputScanner(options);
+
+
+        scanner.setMessage("Choose an option");
+        int answerIndex = prompt.getUserInput(scanner);
+
+        return answerIndex;
+
+    }
+
+    public int clientMenu(){
+        System.out.println("WELCOME TO YOUR PROFILE");
+        String[] options = {"Ask a Driver", "Check Wallet", "Put some money", "Logout"};
+
+        MenuInputScanner scanner = new MenuInputScanner(options);
+        scanner.setMessage("Choose an option");
+
+        int answerIndex = prompt.getUserInput(scanner);
+
+        return answerIndex;
+
+    }
+
 
 }
