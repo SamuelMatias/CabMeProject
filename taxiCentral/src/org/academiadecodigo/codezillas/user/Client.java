4b339977cb2cfa0f9654ff49be6cea909d69c3ee
João Ferreira <joaopsf09@gmail.com>
2019-07-13 10:26:27 +0000

    implemented driver and client classes

--- taxiCentral/src/org/academiadecodigo/codezillas/user/Client.java
+++ taxiCentral/src/org/academiadecodigo/codezillas/user/Client.java
@@ -3,7 +3,48 @@ package org.academiadecodigo.codezillas.user;
     //TODO: Implement user properties and methods
     //FixMe: I suggest using the word "Location" instead of origin to improve readability
 
+import org.academiadecodigo.codezillas.tripManager.Manager;
+
 public class Client {
 
+    private int id;
+    private double wallet;
     private String name;
+    private String location;
+    private String destination;
+
+
+
+    public Client(int id, String name, String location,String destination ){
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.destination = destination;
+    }
+
+    public void callDriver(Manager manager){
+        if (wallet <10){
+            //TODO REFUSE TRIP LOGIC
+        }
+        if ()
+
+
+
+    }
+
+    public void deposit (double cash){
+        wallet += cash;
+
+    }
+
+    public double getWallet(){
+        return wallet;
+
+    }
+
+    public void cabFare(double cash){
+        wallet -= cash;
+
+    }
+
 }
