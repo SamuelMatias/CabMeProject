feeb99f1ecacd9d83bf688dc38dd90e9db3aaab3
Nelson Sa <nelsonysa@gmail.com>
2019-07-13 00:55:30 +0000

    Updated Server and ClientHandler, added ServerMessages

--- taxiCentral/src/org/academiadecodigo/codezillas/tcpServer/ClientHandler.java
+++ taxiCentral/src/org/academiadecodigo/codezillas/tcpServer/ClientHandler.java
@@ -1,10 +1,48 @@
 package org.academiadecodigo.codezillas.tcpServer;
 
+import java.io.*;
 import java.net.Socket;
 
     //TODO: Handle client thread logic
     //Not sure if the name of this class is ideal, up for discussion
 
-public class ClientHandler {
-    private Socket clientSocket;
+public class ClientHandler implements Runnable{
+
+    BufferedReader reader;
+    PrintWriter writer;
+
+    Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket){
+        this.clientSocket = clientSocket;
+    }
+    @Override
+    public void run() {
+        System.out.println("Client handler created");
+        setupStreams();
+        respond();
+
+    }
+
+    public void setupStreams(){
+        try {
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void respond(){
+        try{
+            if(clientSocket.isBound()) {
+                System.out.println("Location");
+                System.out.println(reader.readLine());
+                System.out.println("Destination");
+                System.out.println(reader.readLine());
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
